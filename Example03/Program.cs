c7f84a7e2110daf2e6449497fbd019bfa0f395d1
Burak Avar <burakavar@gmail.com>
2022-04-09 22:06:10 +0300

    example03 added

---
+++ Example03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Example03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example03
+            //Design the algorithm and flowchart to find whether the given
+            //integer is zero, positive, or negative.
+
+            Console.WriteLine("Please enter a number :");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                Console.WriteLine("The number you entered is equal to zero");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number you entered is a negative number.");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine("The number you entered is a positive number");
+            }
+        }
+    }
+}
